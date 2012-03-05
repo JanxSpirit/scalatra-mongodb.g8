51aefba4adfaef9f51061b4ba226778b0c9c657d
Gregg <gregg@wordnik.com>
2012-03-04 21:11:18 -0800

    test update

--- src/main/g8/src/test/scala/TemplateTest.scala
+++ src/main/g8/src/test/scala/TemplateTest.scala
@@ -1,20 +1,26 @@
 import org.scalatra.test.specs2._
 
 object TemplateTest extends ScalatraSpec { def is =
-  "GET /test on $servlet_name$"            ^
+  "GET /test on MongoDBServlet"            ^
     "return status 200"                    !getTestResource^
-  "GET /doesntexist on $servlet_name$"     ^
+  "GET /theFuture on MongoDBServlet"       ^
+    "return staus 200"                     !getFutureResource^
+  "GET /doesntexist on MongoDBServlet"     ^
     "return status 404"                    !getDoesntExistResource^
 				   end
 
-  addServlet(classOf[$servlet_name$], "/*")
+  addServlet(classOf[MongoDBServlet], "/*")
 
   def getTestResource = get("/test") {
     status must_== 200
     body mustEqual("<h1>Test resource</h1>")
   }
 
+  def getFutureResource = get("/theFuture") {
+    status must_== 200
+  } 
+
   def getDoesntExistResource = get("/doesntexist") {
-    status must_==404
+    status must_== 404
   }
 }
